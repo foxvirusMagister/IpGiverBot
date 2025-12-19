d5ef1e6b9d676c29cb3dc5ae8bc4f0596f25b861
foxvirusMagister <nocro.beal.stk.wow@gmail.com>
2025-12-19 21:54:08 +0500

    removed useless hash usings

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 bot = telebot.TeleBot(os.getenv("TOKEN"), parse_mode=None)
-password = hash(os.getenv("PASSWD"))
-owner_name = hash(os.getenv("OWNER"))
-phrase = hash(os.getenv("PHRASE"))
+password = os.getenv("PASSWD")
+owner_name = os.getenv("OWNER")
+phrase = os.getenv("PHRASE")
 try:
     useown = int(os.getenv("USEOWN"))
 except ValueError:
@@ -29,14 +29,14 @@ def check_message(message):
     words = message.text.split(" ")
     try:
         if len(words) == 2:
-            code = hash(words[1])
-            passwd = hash(words[0])
+            code = words[1]
+            passwd = words[0]
             if passwd == password and not useown and code == phrase:
                 ip = subprocess.run(["curl", "ifconfig.me"], capture_output=True, text=True, check=True)
                 bot.reply_to(message, ip.stdout)
         elif len(words) == 1:
-            usrname = hash(message.from_user.username.lower())
-            code = hash(words[0])
+            usrname = message.from_user.username.lower()
+            code = words[0]
             if useown and usrname == owner_name and code == phrase:
                 ip = subprocess.run(["curl", "ifconfig.me"], capture_output=True, text=True, check=True)
                 bot.reply_to(message, ip.stdout)
